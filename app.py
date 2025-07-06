1f5cfb94b8f77c13c9365eb7d4f429829be6ae04
Subhadarshini-cod <subhadarshinirauta@gmail.com>
2025-07-06 09:40:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,url_for
+from user import user_data
+app = Flask (__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html",title="Home",user=user_data)
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title="About",user=user_data)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html",title="contact",user=user_data)
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html",title="projects",user=user_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,69 @@
+user_data = {
+    'message':"Hello every one !!",
+    'intro' : "My name is Subhadarshini Rauta",
+    'profilePic':"#",
+    'bio':(
+    "Hi, I’m Subhadarshini Rauta!"
+"I’m a third-year Computer Science Engineering student at GIET University with a deep interest in both technology and sports. I enjoy creating interactive and user-friendly websites as a passionate web developer, and I also have hands-on experience in SQL development, working with data and databases."
+"As I continue to grow in my tech journey, I’m aiming to become a data analyst, where I can combine my skills in data handling and logical thinking to draw valuable insights and solve real-world problems. Outside of academics, I’m an active sports enthusiast, which helps me stay focused, driven, and team-oriented."
+"I’m always eager to learn, explore new technologies, and take on challenges that help me grow personally and professionally."
+),
+
+    'skills':[  
+        "HTML","CSS","JavaScript","PYTHON","MySQL",
+        "Flask","excel","java"
+
+    ],
+
+    'education' : {
+        '10th':{
+            'school':'st anns convent school',
+             'course':'CBSE',
+        },
+        '12th':{
+            'school': 'SSVM',
+            'course' : 'CHSE',
+        },
+        'Btech':{
+            'school':"GIET",
+            'course':"CSE",
+        }
+    },
+    'achv' :{
+        1:{
+            'pos':'2nd',
+            'prog':"SIH Hakathon",
+        },
+        2:{
+            'pos':'1st',
+            'prog':"web development"
+        }
+    },
+
+    'resume' :"#",
+    'projects':{
+        1:{
+            'name':'Mentor Connect',
+            'tech':'HTML,CSS,JS',
+            "desc":'A mentorship platform where users can find mentors by domain and chat with them,and schedule session'
+        },
+        2:{
+            'name':' RUMEDIC Hospital Management',
+            'tech':'HTML,CSS,JS',
+            "desc":'A hospital management system that allows patients to book appointments and check doctor availability according to different disease.'
+        },
+        3:{
+            'name':'TechArcade : Play Learn LevelUP',
+            'tech':'PYTHON',
+            "desc": 'A innovative web-based platform that combines education with entertaiment to teach technical skills through interactive games'
+        },
+        4:{
+            'name': 'resume builder and email generator',
+             'tech': 'HTML,CSS,PYTHON,ANGULAR,TYPESCRIPT',
+             "desc": 'This application integrates a resume-building tool with an email generator,providing users with a streamlined experience for creating professional resumes and sending tailored emails.'
+        }
+    },
+    'email':'subhadarshinirauta@gmail.com',
+    'github':'https://github.com/Subhadarshini-cod',
+    'linkedin':"https://www.linkedin.com/in/subhadarshini-rauta-577a10332"
+}
